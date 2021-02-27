a6b4b4fffaf85097a1d98362f38654b17fe16ba9
강재현 <wol5971@gmail.com>
2021-02-27 05:16:17 +0000

    initial commit

---
+++ node_4(html_module_server)/4-3/cookie.js
@@ -0,0 +1,10 @@
+const http=require("http");
+
+http.createServer((req, res)=>{
+	console.log(req.url, req.headers.cookie);
+	res.writeHead(200, {"Set-Cookie":"mycookie=test"});
+	res.end("Hello Cookie");
+})
+	.listen(8080, ()=>{
+	console.log("port 8080 waiting");
+})
