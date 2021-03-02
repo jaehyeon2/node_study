81c920758e591e1d542d4c09b0ab0ce6e53f3043
강재현 <wol5971@gmail.com>
2021-03-02 10:29:21 +0000

    commit

--- node_7(MySQL)/7-6/models/index.js
+++ node_7(MySQL)/7-6/models/index.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 module.exports = db;
