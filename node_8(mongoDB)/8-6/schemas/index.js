293bb540a1a023333992a034ea7d9e2f03f38054
강재현 <wol5971@gmail.com>
2021-03-27 07:43:42 +0000

    schemas

---
+++ node_8(mongoDB)/8-6/schemas/index.js
@@ -0,0 +1,28 @@
+const mongoose=require("mongoose");
+
+const connect=()=>{
+	if(process.env.NODE_ENV!=="production"){
+		mongoose.set("debug", true);
+	}
+	mongoose.connect("mongodb://jaehyeon2:rkd-wogus013:27017/admin",{
+		dbName:"nodejs",
+		useNewUrlParser:true,
+		useCreateIndex:true,
+	},(error)=>{
+		if(error){
+			console.log("mongodb connect error!", error);
+		}
+		else{
+			console.log("mongodb connect!");
+		}
+	});
+};
+mongoose.connection.on("error", (error)=>{
+	console.error("mongodb connect error!", error);
+});
+mongoose.connection.on("disconnected", ()=>{
+	console.error("mongodb disconnected! try connect");
+	connect();
+});
+
+module.exports=connect;
